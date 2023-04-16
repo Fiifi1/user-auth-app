006c2f419e5f4c75f9d73fcb60e4c9c254a68201
Fiifi1 <42738798+Fiifi1@users.noreply.github.com>
2023-04-16 20:41:20 +0000

    test: test token verification

---
+++ client-app/src/pages/testPage.js
@@ -0,0 +1,23 @@
+import React, {useState, useEffect} from "react";
+
+function TestComponent (props){
+    const {email} = props;
+    console.log(email);
+    const [is_email, set_email] =  useState(false);
+
+    useEffect (()=>{
+        if (email){
+            set_email(email);
+        }
+    }, [email]);
+
+    return (
+        <div>
+            {is_email &&
+            <h3>The email from the login page is {email}</h3>
+            }
+        </div>
+    );
+}
+
+export default TestComponent;
